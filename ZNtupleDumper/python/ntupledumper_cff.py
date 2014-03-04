25a84b9e81e4448c3153a1cca435a1713c446086
Shervin <shervin@cern.ch>
2014-03-04 17:49:32 +0100

    moved JsonFilter to ZNtupleDumper

--- ZNtupleDumper/python/ntupledumper_cff.py
+++ ZNtupleDumper/python/ntupledumper_cff.py
@@ -1,5 +1,5 @@
 from Calibration.ZNtupleDumper.patSequence_cff import *
 from Calibration.ZNtupleDumper.zntupledumper_cfi import *
-from Calibration.JsonFilter.jsonFilter_cfi import *
+from Calibration.ZNtuperDumper.jsonFilter_cfi import *
 
 
