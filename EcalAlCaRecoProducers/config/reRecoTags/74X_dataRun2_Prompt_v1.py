150a2429b736da25868fa9931594c4b1afc1ab5e
Shervin <shervin@cern.ch>
2015-09-30 14:47:32 +0200

    updated prompt tag

--- EcalAlCaRecoProducers/config/reRecoTags/74X_dataRun2_Prompt_v1.py
+++ EcalAlCaRecoProducers/config/reRecoTags/74X_dataRun2_Prompt_v1.py
@@ -5,5 +5,5 @@ from CondCore.DBCommon.CondDBSetup_cfi import *
 RerecoGlobalTag = cms.ESSource("PoolDBESSource",
                                CondDBSetup,
                                connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
-                               globaltag = cms.string('74X_dataRun2_Prompt_v1'),
+                               globaltag = cms.string('74X_dataRun2_Prompt_v2'),
                                )
